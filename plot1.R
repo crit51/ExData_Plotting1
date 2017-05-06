207d183469d4f52f24f86992606036293e570d00
Seunghak Jin <criteria92@gmail.com>
2017-05-06 16:36:40 +0900

    Assignment

--- plot1.R
+++ plot1.R
@@ -1,9 +1,14 @@
+fileUrl <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+download.file(fileUrl, destfile = "./data/household_power_consumption.zip")
+unzip("./data/household_power_consumption.zip", exdir = "./data")
+
 hpc <- read.table("./data/household_power_consumption.txt", sep=";", header=TRUE, na.strings="?")
+hpc <- hpc[hpc$Date == "1/2/2007" | hpc$Date == "2/2/2007",]
+
 hpc$Date <- as.Date(hpc$Date, format="%d/%m/%Y")
 
 png("plot1.png", width=480, height=480)
-with(hpc[hpc$Date == "2007-02-01" | hpc$Date == "2007-02-02",],
-     hist(Global_active_power, col="red",
-          main="Global Active Power", xlab="Global Active Power (kilowatts)"))
+with(hpc, hist(Global_active_power, col="red",
+               main="Global Active Power", xlab="Global Active Power (kilowatts)"))
 dev.off()
 
--- plot4.R
+++ plot4.R
@@ -1,8 +1,12 @@
+fileUrl <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+download.file(fileUrl, destfile = "./data/household_power_consumption.zip")
+unzip("./data/household_power_consumption.zip", exdir = "./data")
+
 hpc <- read.table("./data/household_power_consumption.txt", sep=";", header=TRUE, na.strings="?")
+hpc <- hpc[hpc$Date == "1/2/2007" | hpc$Date == "2/2/2007",]
+
 hpc$datetime <- paste(hpc$Date, hpc$Time, sep=" ")
-hpc$Date <- as.Date(hpc$Date, format="%d/%m/%Y")
 hpc$datetime <- strptime(hpc$datetime, format="%d/%m/%Y %T")
-hpc <- hpc[hpc$Date == "2007-02-01" | hpc$Date == "2007-02-02",]
 
 png("plot4.png", width=480, height=480)
 par(mfrow=c(2,2))
